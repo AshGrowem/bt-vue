cae983f4fb66dd346dbf48287de4af65f2b8daad
Ashraf Hadden <ashrafhadden@gmail.com>
2019-06-08 19:57:13 -0400

    rm .ag-watemark

--- src/App.vue
+++ src/App.vue
@@ -216,5 +216,9 @@ export default {
 // @import url('https://fonts.googleapis.com/css?family=Exo+2:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i|Titillium+Web:200,200i,300,300i,400,400i,600,600i,700,700i,900|Material+Icons');
 // @import '~vuetify/src/styles/main.sass';
 @import '~vuetify/dist/vuetify.min.css';
+
+.ag-watermark {
+  display: none !important;
+}
 </style>
 ───────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
